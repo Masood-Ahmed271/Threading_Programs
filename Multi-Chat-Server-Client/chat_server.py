8dc53a36083431a473b6b241adfb15270623d066
Masood Ahmed <mangimasood2000@gmail.com>
2023-03-03 15:53:16 -0500

    added and cleaned some comments

--- Multi-Chat-Server-Client/chat_server.py
+++ Multi-Chat-Server-Client/chat_server.py
@@ -6,53 +6,47 @@
         The server will be able to handle multiple clients at the same time.
     Author:
         Masood Ahmed
-    Student Number:
-        20379714
 
 '''
 
 import socket
 import threading
 
-# TODO: create an empty list that will store 
 clients = [] # new clients will be added to this list
 
-# TODO: Complete the implementation of the class named ClientThread that will
 # contain the logic of each client. Each client should
 # be able to exchange messages with the server. 
-# changed the name from ClassThread to ClientThread as asked on line 8
 class ClientThread(threading.Thread):
     def __init__(self, conn, addr):
         super().__init__()
         self.conn = conn
         self.addr = addr
     
-    #The function will contain the logic of each client
-    #connected to the server. "run()" function will be called after you create 
-    #a new instance of the ClientThread and calling the "start()" function.
+    # The function will contain the logic of each client
+    # connected to the server. "run()" function will be called after you create 
+    # a new instance of the ClientThread and calling the "start()" function.
     def run(self):
         while True:
-            # TODO: remove the "pass" keyword before modifiying this function
 
-            # TODO: receive data stream from client. 
+            # receive data stream from client. 
             # it won't accept data packet greater than 1024 bytes
             data = self.conn.recv(1024).decode()
 
-            # TODO: if no data is received from the client, break this loop
+            # if no data is received from the client, break this loop
             if not data:
                 break
 
-            # TODO: print the data received from the client
+            # print the data received from the client
             print("from connected user: " + str(data))
 
-            # TODO: now it's the server's turn to reply to the client's message.
+            # now it's the server's turn to reply to the client's message.
             # take input from the console screen using "input()" function then, send
             # the message to the client using "self.conn.send()" function. 
             reply = input(' -> ')
             self.conn.send(reply.encode())
         
-        # TODO: close the connection "self.conn" and remove it from the list
-        #that stores all the client's connections. 
+        # close the connection "self.conn" and remove it from the list
+        # that stores all the client's connections. 
         self.conn.close()
         clients.remove(self.conn)
 
@@ -66,7 +60,7 @@ def server_program():
     port = 5000  # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
+    # The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
@@ -77,13 +71,13 @@ def server_program():
         conn, address = server_socket.accept()  # accept new connection
         print("Connection from: " + str(address))
 
-        # TODO: create a new instance of ClientThread
+        # create a new instance of ClientThread
         new_client = ClientThread(conn, address)
 
-        # TODO: add the newly created instance to the client's list
+        # add the newly created instance to the client's list
         clients.append(new_client)
 
-        # TODO: start the thread of the newly created instance of 
+        # start the thread of the newly created instance of 
         # ClientThread using "start()" function.
         new_client.start() 
 
--- Producer-Consumer-Problem/prod_cons.py
+++ Producer-Consumer-Problem/prod_cons.py
@@ -1,8 +1,9 @@
 '''
-  Course: CISC 324 - Operating Systems
-  Lab 2: Producer-Consumer Problem
-  Name: Masood Ahmed
-  Student Number: 20379714
+  Description: 
+      Simulation of Producer Consumer Problem using Semaphores
+  Author: 
+      Masood Ahmed
+
 '''
 import threading
 import time
@@ -16,11 +17,10 @@ out_index = 0
 n_producers = 5
 n_consumers = 5
 
-barrier = threading.Barrier(n_producers)# This barrier is used to make sure that all producers finish at the same time. You don't need to worry about it. 
+barrier = threading.Barrier(n_producers)# This barrier is used to make sure that all producers finish at the same time. 
 
-# TODO: Declare the required semaphores here (mutex, empty, full) and initialize them with the proper values.
-#  You will need to add mark these semaphores as global in both the Producer and Consumer classes (see the code below)
-# Declaring Semaphores
+#  Declare the required semaphores here (mutex, empty, full) and initialize them with the proper values.
+#  Declaring Semaphores
 mutex = threading.Semaphore() # default value is 1
 empty = threading.Semaphore(CAPACITY)
 full = threading.Semaphore(0)
@@ -34,16 +34,16 @@ class Producer(threading.Thread):
     def run(self, id=0):
 
         global CAPACITY, buffer, in_index, out_index, barrier, finished
-        #TODO: mark the semaphores as global here
+        # mark the semaphores as global here
         global mutex, empty, full
 
         items_produced = 0
         counter = 0
 
         while items_produced < 20:
-            # TODO: acquire one of the declared semaphores here
+            # acquire one of the declared semaphores here
             empty.acquire()
-            # TODO: acquire one of the declared semaphores here
+            # acquire one of the declared semaphores here
             mutex.acquire()
 
             counter += 1
@@ -51,7 +51,7 @@ class Producer(threading.Thread):
             in_index = (in_index + 1) % CAPACITY
             print(f"Producer {self.id} produced : ", counter)
 
-            # TODO: don't forget to release the semaphores here accordingly
+            # release the semaphores here accordingly
             mutex.release()
             full.release()
 
@@ -76,16 +76,16 @@ class Consumer(threading.Thread):
 
   def run(self):
     global CAPACITY, buffer, in_index, out_index, counter
-    #TODO: mark the semaphores as global here
+    # mark the semaphores as global here
     global mutex, empty, full
 
     items_consumed = 0
     while True:
       if finished == True:# if all producers are done, then we can stop consuming
         break;
-      # TODO: use one of the declared semaphores here
+      # use one of the declared semaphores here
       full.acquire()
-      # TODO: use one of the declared semaphores here
+      # use one of the declared semaphores here
       mutex.acquire()
 
       item = buffer[out_index]
@@ -93,7 +93,7 @@ class Consumer(threading.Thread):
 
       print(f"Consumer {self.id} consumed item : ", item)
 
-      # TODO: don't forget to release the semaphores here accordingly
+      # release the semaphores here accordingly
       mutex.release()
       empty.release()  
 
@@ -107,25 +107,25 @@ class Consumer(threading.Thread):
 
 # Creating Threads
 
-producers = [Producer(id=i) for i in range(n_producers)] # TODO: Modify this line accordingly to create n_producers threads
-consumers = [Consumer(id=i) for i in range(n_consumers)] # TODO: Modify this line accordingly to create n_consumers threads
+producers = [Producer(id=i) for i in range(n_producers)] # to create n_producers threads
+consumers = [Consumer(id=i) for i in range(n_consumers)] # to create n_consumers threads
 
 
 # start the producers
 # Starting Threads
-# TODO: Loop through the producers and start them
+# Loop through the producers and start them
 for producer in producers:
   producer.start()
 
-# TODO: Loop through the consumers and start them
+# Loop through the consumers and start them
 for consumer in consumers:
   consumer.start()
 
 # Waiting for threads to complete
-#TODO: loop through the producers and join them using join() function
+# loop through the producers and join them using join() function
 for producer in producers:
   producer.join()
-#TODO: loop through the consumers and join them using join() function
+# loop through the consumers and join them using join() function
 for consumer in consumers:
   consumer.join()
 
